86aa4d653d2baaffbcdad2e08878ccdcbf8e74f1
David Berezowski <david.berezowski1@gmail.com>
2025-05-11 15:11:59 +0200

    Added "site under construction" disclaimer

--- src/components/layout/page/Footer.tsx
+++ src/components/layout/page/Footer.tsx
@@ -4,7 +4,7 @@ export default function Footer() {
 
     return (
         <div className={'py-0.5 px-1 flex flex-row gap-2 items-end text-neumorphic-500 dark:text-neumorphic-400'}>
-            time-to-work
+            time-to-work | PREVIEW | Seite in Entwicklung, es kann zu Fehlern kommen
 
             <Hyperlink className={'ml-auto text-sm font-[350]'} href={"https://www.bdav.dev/datenschutzerklaerung"}>
                 Datenschutzerklärung
