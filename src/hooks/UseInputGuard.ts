7010e98260bdfef95aeedd4192c2fe26d88f7c4e
David Berezowski <david.berezowski1@gmail.com>
2025-05-25 18:26:28 +0200

    Added input guard

---
+++ src/hooks/UseInputGuard.ts
@@ -0,0 +1,33 @@
+import { Dispatch, SetStateAction, useEffect, useMemo, useState } from "react";
+
+
+type GuardValue<T> = { fallback?: never, value: T } | { fallback: T, value?: never };
+
+export default function useInputGuard<I, V>(
+    guard: (inputValue: I, value: V) => GuardValue<V>,
+    encode: (value: V) => I,
+    state: [V, (value: V) => void]
+): [I, Dispatch<SetStateAction<I>>, () => void, boolean] {
+    const [value, setValue] = state;
+    const [inputValue, setInputValue] = useState<I>(encode(value));
+    const guardValue = useMemo(() => guard(inputValue, value), [inputValue]);
+    const isInputValid = () => "value" in guardValue;
+
+    useEffect(() => {
+        setInputValue(encode(value));
+    }, [value]);
+
+    useEffect(() => {
+        if (isInputValid()) {
+            setValue(guardValue.value!);
+        }
+    }, [guardValue]);
+
+    const applyFallbackIfInputInvalid = () => {
+        if (!isInputValid()) {
+            setInputValue(encode(guardValue.fallback!));
+        }
+    }
+
+    return [inputValue, setInputValue, applyFallbackIfInputInvalid, !isInputValid()];
+}
