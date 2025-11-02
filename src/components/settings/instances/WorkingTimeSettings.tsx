5beda9c36d862adc1f8918a499603b4475a89831
David Berezowski <david.berezowski1@gmail.com>
2025-11-02 12:19:52 +0100

    Added work time regulation settings

--- src/components/settings/instances/WorkingTimeSettings.tsx
+++ src/components/settings/instances/WorkingTimeSettings.tsx
@@ -4,6 +4,9 @@ import useMutatingConfigurationValue from "@/hooks/configuration/UseMutatingConf
 import SignedTimeSpanPicker from "@/components/primitives/control/SignedTimeSpanPicker";
 import TimeSpanPicker from "@/components/primitives/control/TimeSpanPicker";
 import Toggle from "@/components/primitives/control/Toggle";
+import useBrowserNotifications from "@/hooks/UseBrowserNotifications";
+import Checkbox from "@/components/primitives/control/Checkbox";
+import useInputGuard from "@/hooks/UseInputGuard";
 
 
 export default function WorkingTimeSettings() {
@@ -11,6 +14,30 @@ export default function WorkingTimeSettings() {
     const [timeBalance, setTimeBalance] = useMutatingConfigurationValue(config => config.workingTime.timeBalance);
     const [minBreak, setMinBreak] = useMutatingConfigurationValue(config => config.workingTime.minBreak);
     const [showOptimalBreakTime, setShowOptimalBreakTime] = useMutatingConfigurationValue(config => config.workingTime.showOptimalBreakTime);
+    const [maxWorkTimeBlockViolationNotificationThreshold, setMaxWorkTimeBlockViolationNotificationThreshold] = useMutatingConfigurationValue(config => config.workingTime._impendingWorkTimeViolation.threshold);
+    const [notifyOnImpendingMaxWorkTimeBlockViolation, setNotifyOnImpendingMaxWorkTimeBlockViolation] = useMutatingConfigurationValue(config => config.workingTime._impendingWorkTimeViolation.notify);
+    const [maxWorkTimeBlockDuration, setMaxWorkTimeBlockDuration, applyMaxWorkTimeBlockDurationFallback] =
+        useInputGuard<TimeSpan | undefined, TimeSpan | undefined>({
+            guard: inputValue => {
+                if (inputValue?.equals(TimeSpan.zero())) {
+                    return { fallback: undefined };
+                }
+                return { value: inputValue };
+            },
+            encode: s => s,
+            state: useMutatingConfigurationValue(config => config.workingTime.maxWorkTimeBlockDuration)
+        });
+
+    const browserNotification = useBrowserNotifications();
+
+    const isViolationNotificationInputDisabled = !maxWorkTimeBlockDuration || !notifyOnImpendingMaxWorkTimeBlockViolation;
+
+    function askForNotificationPermission(wasChecked: boolean) {
+        if (!wasChecked || browserNotification.availability.value != "default") {
+            return;
+        }
+        browserNotification.requestPermission();
+    }
 
     return (
         <div>
@@ -23,7 +50,7 @@ export default function WorkingTimeSettings() {
                                 setting: <TimeSpanPicker
                                     value={dailyWorkingTime}
                                     onValueChange={setDailyWorkingTime}
-                                    invalid={!dailyWorkingTime || dailyWorkingTime.equals(TimeSpan.empty())}
+                                    invalid={!dailyWorkingTime || dailyWorkingTime.equals(TimeSpan.zero())}
                                 />
                             },
                             {
@@ -37,7 +64,7 @@ export default function WorkingTimeSettings() {
                         ]
                     },
                     {
-                        title: "Einschränkungen",
+                        title: "Pausenregulierungen",
                         settings: [
                             {
                                 label: "Mindestpause",
@@ -50,6 +77,41 @@ export default function WorkingTimeSettings() {
                                 label: "Zeige Ende von optimaler Pause auf Timeline",
                                 setting: disabled => <Toggle disabled={disabled} value={!minBreak ? false : showOptimalBreakTime} onValueChange={setShowOptimalBreakTime}/>,
                                 disabled: !minBreak
+                            }
+                        ]
+                    },
+                    {
+                        title: "Arbeitszeitregulierungen",
+                        settings: [
+                            {
+                                label: "Maximale Arbeitszeit am Stück",
+                                setting: <TimeSpanPicker
+                                    value={maxWorkTimeBlockDuration}
+                                    onValueChange={setMaxWorkTimeBlockDuration}
+                                    onBlur={applyMaxWorkTimeBlockDurationFallback}
+                                />
+                            },
+                            {
+                                label: "Benachrichtige bei bevorstehender Überschreitung",
+                                disabled: !maxWorkTimeBlockDuration,
+                                setting: disabled => <>
+                                    <Checkbox
+                                        disabled={disabled}
+                                        value={!disabled && notifyOnImpendingMaxWorkTimeBlockViolation}
+                                        setValue={value => {
+                                            setNotifyOnImpendingMaxWorkTimeBlockViolation(value);
+                                            if (value) askForNotificationPermission(value);
+                                        }}
+                                    />
+                                    <span className={`${isViolationNotificationInputDisabled && 'opacity-60'} mr-0.5`}>Benachrichtige</span>
+                                    <TimeSpanPicker
+                                        value={maxWorkTimeBlockViolationNotificationThreshold}
+                                        onValueChange={setMaxWorkTimeBlockViolationNotificationThreshold}
+                                        disabled={isViolationNotificationInputDisabled}
+                                        invalid={notifyOnImpendingMaxWorkTimeBlockViolation && !maxWorkTimeBlockViolationNotificationThreshold}
+                                    />
+                                    <span className={`${isViolationNotificationInputDisabled && 'opacity-60'} ml-0.5`}>davor</span>
+                                </>
                             }
                         ]
                     }
